9293cbb4d2bfb2ffb33662449fd9a4e2ea0512c0
Josh Picarazzi <joshpicarazzi@gmail.com>
2023-11-08 22:11:42 -0600

    Setting up structure for code base

--- scripts/Controller.gd
+++ scripts/Controller.gd
@@ -1,9 +1,11 @@
-extends AnimatedSprite2D
+extends Node
 
+var gameState
+var randomNum
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomNum.randomize()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
